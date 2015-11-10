d980ec93765338032fbf3ac896087e39c83d7d11
S <s>
2015-11-10 13:49:37 -0800

    {"type":"TI", "runs":"1"}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -56,7 +56,7 @@ public class GraphicsContest extends GraphicsProgram {
 			removeAll();
 			add(tobinLine);
 			try {
-				Thread.sleep(1);
+				Thread.sleep(5);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
