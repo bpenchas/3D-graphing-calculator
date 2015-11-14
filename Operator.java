bbe65db9b623c079d73eeedf98b02064c453786e
S <s>
2015-11-13 16:51:28 -0800

    {"type":"TI", "runs":"0"}

--- Operator.java
+++ Operator.java
@@ -1,4 +1,22 @@
 
 public enum Operator {
-
+	ADD, SUBTRACT, MULTIPLY, DIVIDE, EXPONENT, OPAREN, CPAREN
+	
+	public int precedence(String operator) {
+		switch(operator) {
+			case "+":
+				return 1;
+			case "-":
+				return 1;
+			case "*":
+				return 2;
+			case "/":
+				return 2;
+			case "^":
+				return 3;
+			default:
+				return 0;
+				
+		}
+	}
 }
