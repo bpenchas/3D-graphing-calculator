23c2011daf6726717fb652e9deb439c40c63db3a
S <s>
2015-11-14 12:11:10 -0800

    {"type":"TI", "runs":"0"}

--- Operator.java
+++ Operator.java
@@ -2,22 +2,44 @@
 public enum Operator {
 	ADD, SUBTRACT, MULTIPLY, DIVIDE, EXPONENT, OPAREN, CPAREN, NOOP;
 	
-	public int precedence() {
+	public int stackPrecedence() {
+		switch(this) {
+			case ADD:
+				return 2;
+			case SUBTRACT:
+				return 2;
+			case MULTIPLY:
+				return 4;
+			case DIVIDE:
+				return 4;
+			case EXPONENT:
+				return 5;
+			case OPAREN:
+				return 0;
+			case CPAREN:
+				return 99;
+			default:
+				return 0;
+				
+		}
+	}
+	
+	public int inputPrecedence() {
 		switch(this) {
 			case ADD:
 				return 1;
 			case SUBTRACT:
 				return 1;
 			case MULTIPLY:
-				return 2;
-			case DIVIDE:
-				return 2;
-			case EXPONENT:
 				return 3;
+			case DIVIDE:
+				return 3;
+			case EXPONENT:
+				return 6;
 			case OPAREN:
-				return 4;
+				return 100;
 			case CPAREN:
-				return 5;
+				return 0;
 			default:
 				return 0;
 				
