0767b33bed089a350aa41c8ba0bdb184da8edbdc
S <s>
2015-11-16 22:33:34 -0800

    {"type":"TI", "runs":"0"}

--- Value.java
+++ Value.java
@@ -5,7 +5,7 @@ public enum Value implements Element {
 	public double constValue = 0;
 	
 	
-	public Value fromString(String token) {
+	public static Value fromString(String token) {
 		if (token.matches("[0-9]+")) {
 			Value newValue = CONST;
 			newValue.constValue = Double.parseDouble(token);
