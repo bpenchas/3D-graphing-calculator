c39bf954ea8cfb2e22b9fb2ef0c83dbc909168b7
S <s>
2015-11-17 15:05:41 -0800

    {"type":"TI", "runs":"1"}

--- Rect3D.java
+++ Rect3D.java
@@ -28,7 +28,8 @@ public class Rect3D {
 		}
 		GPolygon tempPolygon = new GPolygon(new GPoint[] {tempCornerOne, tempCornerTwo, tempCornerThree, tempCornerFour});
 		tempPolygon.setFilled(true);
-		tempPolygon.setFillColor(new Color(255 / (int) (cornerOne.getZ() + 1), 255 / (int) (cornerOne.getZ() + 1), 255 / (int) (cornerOne.getZ() + 1)));
+		tempPolygon.setFillColor(Color.RED);
+		//tempPolygon.setFillColor(new Color(255 / (int) (cornerOne.getZ() + 1), 255 / (int) (cornerOne.getZ() + 1), 255 / (int) (cornerOne.getZ() + 1)));
 		return tempPolygon;
 		
 	}
