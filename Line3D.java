f41b3f31497f6de79a97cf800e9019f008af4bf6
S <s>
2015-11-16 23:13:32 -0800

    {"type":"TI", "runs":"0"}

--- Line3D.java
+++ Line3D.java
@@ -1,4 +1,25 @@
 
-public class Line3D {
+import acm.graphics.GLine;
+import acm.graphics.GPoint;
 
-}
+
+public class Line3D {
+		private Point3D first;
+		private Point3D second;
+		private GLine lineFromPoints(GPoint first, GPoint second) {
+			return new GLine(first.getX(), first.getY(), second.getX(), second.getY());
+		}
+
+		private GLine to2D() {
+			return lineFromPoints(first.to2D(), second.to2D());
+		}
+		
+		public Line3D(Point3D first, Point3D second) {
+			this.first = first;
+			this.second = second;
+		}
+		
+		public Line3D rotate(){
+			return new Line3D(first.rotate(theta, phi), second.rotate(theta, phi));
+		}
+	}
