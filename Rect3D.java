0042eeec2a81fb854c86f47731c79f7f36d1a097
S <s>
2015-11-16 23:25:28 -0800

    {"type":"TI", "runs":"0"}

--- Rect3D.java
+++ Rect3D.java
@@ -1,12 +1,23 @@
+import acm.graphics.GPolygon;
+
 
 public class Rect3D {
 	Point3D cornerOne;
 	Point3D cornerTwo;
-	Point3D cornerThird;
+	Point3D cornerThree;
 	Point3D cornerFour;
 	
-	public Rect3D(Point3D cornerOne, Point3D cornerTwo, Point3D cornerThird, Point3D cornerFour) {
+	public Rect3D(Point3D cornerOne, Point3D cornerTwo, Point3D cornerThree, Point3D cornerFour) {
+		this.cornerOne = cornerOne;
+		this.cornerTwo = cornerTwo;
+		this.cornerThree = cornerThree;
+		this.cornerFour = cornerFour;
 		
+	}
+	
+	private GPolygon to2D() {
+		GPolygon rect = 
+				new GPolygon(new GPoint[] {this.cornerOne.to2D(), this.cornerTwo.to2D(), this.cornerThree.to2D(), this.cornerFour.to2D()});
 	}
 
 }
