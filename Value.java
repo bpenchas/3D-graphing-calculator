3898e280aee1fc12bed2839e986cda02cbcecb6e
S <s>
2015-11-16 22:30:45 -0800

    {"type":"TI", "runs":"0"}

--- Value.java
+++ Value.java
@@ -1,4 +1,22 @@
 
 public enum Value {
+	CONST, X, Y;
+	
+	public double constValue = 0;
+	
+	
+	public Value fromString(String token) {
+		if (token.matches("[0-9]+")) {
+			Value newValue = CONST;
+			newValue.constValue = Double.parseDouble(token);
+			return newValue;
+		} else if (token.matches("[xX]")) {
+			return X;
+		} else if (token.matches("[yY]")) {
+			return Y;
+		} else {
+			return null;
+		}
+	}
 
 }
