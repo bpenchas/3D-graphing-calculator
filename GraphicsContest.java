4bb776397c004ffddd35a76d75e79b3d81648f3a
S <s>
2015-11-17 17:25:39 -0800

    {"type":"TI", "runs":"1"}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -183,7 +183,7 @@ public class GraphicsContest extends GraphicsProgram {
 			
 			for (int i = 0; i < rectArray.length; i ++) {
 				for (int j = 0; j < rectArray.length; j ++) {
-					GPolygon temp = rectArray[i][j].to2D();
+					GPolygon temp = rectArray[i][j].rotate(theta, phi).to2D();
 					if (temp != null) {
 						add(temp);
 					}
