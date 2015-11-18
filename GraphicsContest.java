e350ed4f916fa1956834e6c64564c71ac4f1ac07
S <s>
2015-11-17 17:20:55 -0800

    {"type":"TI", "runs":"1"}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -169,9 +169,14 @@ public class GraphicsContest extends GraphicsProgram {
 //			add(yAxis.rotate(theta, phi).to2D());
 //			add(zAxis.rotate(theta, phi).to2D());
 			
-			xAxis.to2D().setLocation(xAxis.rotate(theta, phi).to2D().getLocation());
-			yAxis.to2D().setLocation(yAxis.rotate(theta, phi).to2D().getLocation());
-			zAxis.to2D().setLocation(zAxis.rotate(theta, phi).to2D().getLocation());
+			xAxis.to2D().setStartPoint(xAxis.rotate(theta, phi).to2D().getStartPoint().getX(), xAxis.rotate(theta, phi).to2D().getStartPoint().getY());
+			xAxis.to2D().setEndPoint(xAxis.rotate(theta, phi).to2D().getEndPoint().getX(), xAxis.rotate(theta, phi).to2D().getEndPoint().getY());
+			
+			yAxis.to2D().setStartPoint(yAxis.rotate(theta, phi).to2D().getStartPoint().getX(), yAxis.rotate(theta, phi).to2D().getStartPoint().getY());
+			yAxis.to2D().setEndPoint(yAxis.rotate(theta, phi).to2D().getEndPoint().getX(), yAxis.rotate(theta, phi).to2D().getEndPoint().getY());
+			
+			zAxis.to2D().setStartPoint(zAxis.rotate(theta, phi).to2D().getStartPoint().getX(), zAxis.rotate(theta, phi).to2D().getStartPoint().getY());
+			zAxis.to2D().setEndPoint(zAxis.rotate(theta, phi).to2D().getEndPoint().getX(), zAxis.rotate(theta, phi).to2D().getEndPoint().getY());
 			
 			
 			for (int i = 0; i < rectArray.length; i ++) {
