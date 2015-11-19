5c878f4852495a51158296668b5a85c375423e00
S <s>
2015-11-19 12:13:32 -0800

    {"type":"TI", "runs":"0"}

--- Rect3D.java
+++ Rect3D.java
@@ -10,6 +10,11 @@ public class Rect3D {
 	Point3D cornerThree;
 	Point3D cornerFour;
 	
+	GPoint cornerOne2D;
+	GPoint cornerTwo2D;
+	GPoint cornerThree2D;
+	GPoint cornerFour2D;
+	
 	public Rect3D(Point3D cornerOne, Point3D cornerTwo, Point3D cornerThree, Point3D cornerFour) {
 		this.cornerOne = cornerOne;
 		this.cornerTwo = cornerTwo;
@@ -19,18 +24,18 @@ public class Rect3D {
 	}
 	
 	public GPolygon to2D() {
-		GPoint tempCornerOne = this.cornerOne.to2D();
-		GPoint tempCornerTwo = this.cornerTwo.to2D();
-		GPoint tempCornerThree = this.cornerThree.to2D();
-		GPoint tempCornerFour = this.cornerFour.to2D();
-		if (tempCornerOne == null || tempCornerTwo == null || tempCornerThree == null || tempCornerFour == null) {
-			return null;
-		}
-		GPolygon tempPolygon = new GPolygon(new GPoint[] {tempCornerOne, tempCornerTwo, tempCornerThree, tempCornerFour});
-		tempPolygon.setFilled(true);
-		tempPolygon.setFillColor(Color.RED);
+//		GPoint tempCornerOne = this.cornerOne.to2D();
+//		GPoint tempCornerTwo = this.cornerTwo.to2D();
+//		GPoint tempCornerThree = this.cornerThree.to2D();
+//		GPoint tempCornerFour = this.cornerFour.to2D();
+//		if (tempCornerOne == null || tempCornerTwo == null || tempCornerThree == null || tempCornerFour == null) {
+//			return null;
+//		}
+		return new GPolygon(new GPoint[] {cornerOne2D, cornerTwo2D, cornerThree2D, cornerFour2D});
+//		tempPolygon.setFilled(true);
+//		tempPolygon.setFillColor(Color.RED);
 		//tempPolygon.setFillColor(new Color(255 / (int) (cornerOne.getZ() + 1), 255 / (int) (cornerOne.getZ() + 1), 255 / (int) (cornerOne.getZ() + 1)));
-		return tempPolygon;
+		
 		
 	}
 	
