3e68e7785b8cec6897368ed3acc050c65ba866d5
S <s>
2015-11-19 12:35:37 -0800

    {"type":"TI", "runs":"2"}

--- Rect3D.java
+++ Rect3D.java
@@ -47,6 +47,7 @@ public class Rect3D {
 	
 	public void rotate(double theta, double phi){
 		//cornerOne2D.setLocation(cornerOne.rotate(theta, phi).to2D());
+		polygon.rotate(0.01);
 		cornerOne2D.setLocation(0, 0);
 		cornerTwo2D.setLocation(cornerTwo.rotate(theta, phi).to2D());
 		cornerThree2D.setLocation(cornerThree.rotate(theta, phi).to2D());
