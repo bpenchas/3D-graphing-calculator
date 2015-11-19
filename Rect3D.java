1ca59d8d52ba4aba0d6f35e059b4be8d57529490
S <s>
2015-11-19 12:35:10 -0800

    {"type":"TI", "runs":"3"}

--- Rect3D.java
+++ Rect3D.java
@@ -14,6 +14,7 @@ public class Rect3D {
 	GPoint cornerTwo2D;
 	GPoint cornerThree2D;
 	GPoint cornerFour2D;
+	GPolygon polygon;
 	
 	public Rect3D(Point3D cornerOne, Point3D cornerTwo, Point3D cornerThree, Point3D cornerFour) {
 		this.cornerOne = cornerOne;
@@ -24,6 +25,7 @@ public class Rect3D {
 		this.cornerTwo2D = cornerTwo.to2D();
 		this.cornerThree2D = cornerThree.to2D();
 		this.cornerFour2D = cornerFour.to2D();
+		this.polygon = new GPolygon(new GPoint[] {cornerOne2D, cornerTwo2D, cornerThree2D, cornerFour2D});
 	}
 	
 	public GPolygon to2D() {
@@ -34,11 +36,12 @@ public class Rect3D {
 //		if (tempCornerOne == null || tempCornerTwo == null || tempCornerThree == null || tempCornerFour == null) {
 //			return null;
 //		}
-		return new GPolygon(new GPoint[] {cornerOne2D, cornerTwo2D, cornerThree2D, cornerFour2D});
+//		GPolygon temp = new GPolygon(new GPoint[] {cornerOne2D, cornerTwo2D, cornerThree2D, cornerFour2D});
+//		
 //		tempPolygon.setFilled(true);
 //		tempPolygon.setFillColor(Color.RED);
 		//tempPolygon.setFillColor(new Color(255 / (int) (cornerOne.getZ() + 1), 255 / (int) (cornerOne.getZ() + 1), 255 / (int) (cornerOne.getZ() + 1)));
-		
+		return this.polygon;
 		
 	}
 	
