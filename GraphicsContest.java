96ea5e30f1acc6bf9bcf4810edf85920c21bab12
S <s>
2015-11-23 10:47:56 -0800

    {"type":"TI", "runs":"0"}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -125,11 +125,11 @@ public class GraphicsContest extends GraphicsProgram {
 	
 	private void convertToPostfix() {
 		IODialog dialog = getDialog();
-		dialog.println("Welcome to the 3D Graphing Calculator!");
-		dialog.print("1. Enter an equation in terms of X and Y ( Z = your equation)");
-		dialog.print("2. The following operators are permitted: ^, *, /, + -");
-		dialog.print("3. The function will be drawn in 3D. Click and drag to explore the function in 3D");
-		dialog.print("4. Use the zoom buttons to change the viewing distance from the graph");
+//		dialog.println("Welcome to the 3D Graphing Calculator!");
+//		dialog.print("1. Enter an equation in terms of X and Y ( Z = your equation)");
+//		dialog.print("2. The following operators are permitted: ^, *, /, + -");
+//		dialog.print("3. The function will be drawn in 3D. Click and drag to explore the function in 3D");
+//		dialog.print("4. Use the zoom buttons to change the viewing distance from the graph");
 		String equation = dialog.readLine("Z = ");
 		
 		StringTokenizer st = new StringTokenizer(equation, DELIMITERS, true);
