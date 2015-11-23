5c7435cad0758d33714aa21211413c34defcbaa8
S <s>
2015-11-23 14:01:17 -0800

    {"type":"TI", "runs":"1"}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -151,9 +151,9 @@ public class GraphicsContest extends GraphicsProgram {
 		IODialog dialog = getDialog();
 		dialog.print("Welcome to the 3D Graphing Calculator!");
 		dialog.print("\n1. Enter an equation in terms of X and Y ( Z = your equation)");
-		dialog.print("2. The following operators are permitted: ^, *, /, + -");
-		dialog.print("3. The function will be drawn in 3D. Click and drag to explore the function in 3D");
-		dialog.print("4. Use the zoom buttons to change the viewing distance from the graph");
+		dialog.print("\n2. The following operators are permitted: ^, *, /, + -");
+		dialog.print("\n3. The function will be drawn in 3D. Click and drag to explore the function in 3D");
+		dialog.println("\n4. Use the zoom buttons to change the viewing distance from the graph");
 		String tempEquation = dialog.readLine("Z = ");
 		equation = tempEquation.replaceAll("\\s+","");
 		System.out.println(equation);
