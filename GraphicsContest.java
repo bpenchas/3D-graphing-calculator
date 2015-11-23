93518c1744bfc920665ef8a2974e4dcc081dc17c
S <s>
2015-11-23 13:57:47 -0800

    {"type":"TI", "runs":"1"}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -226,10 +226,11 @@ public class GraphicsContest extends GraphicsProgram {
 		
 		for (int i = 0; i < resolution; i ++) {
 			
-			if(colorCounterTwo > 10) colorCounterTwo--;
+			
 			if(colorCounterThree < 250) colorCounterThree++;
 			for (int j = 0; j < resolution; j ++) {
 				if(colorCounter < 250) colorCounter++;
+				if(colorCounterTwo > 10) colorCounterTwo--;
 				lineArray[i][j][0] = new Line3D(pointArray[i][j], pointArray[i][j + 1], colorCounter);
 				lineArray[i][j][1] = new Line3D(pointArray[i][j], pointArray[i + 1][j], colorCounter);
 				Color current = new Color(colorCounter, colorCounterTwo, colorCounterThree);
