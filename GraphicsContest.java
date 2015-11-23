b59154bd8abcdda8ce22c5a69dacd0371693efd1
S <s>
2015-11-23 14:06:11 -0800

    {"type":"TI", "runs":"1"}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -74,9 +74,9 @@ public class GraphicsContest extends GraphicsProgram {
 	public double theta = 0;
 	public double phi = 0;
 	
-	public int colorCounter = 20;
-	public int colorCounterTwo = 250;
-	public int colorCounterThree = 80;
+	public int colorCounter = 50;
+	public int colorCounterTwo = 200;
+	public int colorCounterThree = 100;
 	
 	private String DELIMITERS = "+-*/^()xXyY";
 	
