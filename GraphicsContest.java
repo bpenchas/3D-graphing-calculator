fd9ff945dad48fb580b90213bc1967fa24a0b560
S <s>
2015-11-23 13:59:47 -0800

    {"type":"TI", "runs":"0"}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -110,9 +110,9 @@ public class GraphicsContest extends GraphicsProgram {
 		Point3D x = new Point3D(10, 0, 0);
 		Point3D origin = new Point3D(0, 0, 0);
 		
-		Line3D xAxis = new Line3D(origin, x, colorCounter);
-		Line3D yAxis = new Line3D(origin, y, colorCounter);
-		Line3D zAxis = new Line3D(origin, z, colorCounter);
+		Line3D xAxis = new Line3D(origin, x);
+		Line3D yAxis = new Line3D(origin, y);
+		Line3D zAxis = new Line3D(origin, z);
 		
 		add(xAxis.to2D());
 		add(yAxis.to2D());
