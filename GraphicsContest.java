bd4e87d3f01cda0eee6216e7394db86324a380e6
S <s>
2015-11-23 13:49:46 -0800

    {"type":"TI", "runs":"0"}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -229,7 +229,7 @@ public class GraphicsContest extends GraphicsProgram {
 				
 				lineArray[i][j][0] = new Line3D(pointArray[i][j], pointArray[i][j + 1], colorCounter);
 				lineArray[i][j][1] = new Line3D(pointArray[i][j], pointArray[i + 1][j], colorCounter);
-				Color current = new Color(colorCounter, colorCounter, colorCounter);
+				Color current = new Color(colorCounter, 150, 150);
 				lineArray[i][j][0].to2D().setColor(current);
 				lineArray[i][j][1].to2D().setColor(current);
 				add(lineArray[i][j][0].to2D());
