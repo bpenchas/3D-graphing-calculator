f8744f6580fbf716f23bb60ec5980f4dbd207725
S <s>
2015-11-23 13:53:48 -0800

    {"type":"TI", "runs":"1"}

--- GraphicsContest.java
+++ GraphicsContest.java
@@ -150,8 +150,8 @@ public class GraphicsContest extends GraphicsProgram {
 //		dialog.print("2. The following operators are permitted: ^, *, /, + -");
 //		dialog.print("3. The function will be drawn in 3D. Click and drag to explore the function in 3D");
 //		dialog.print("4. Use the zoom buttons to change the viewing distance from the graph");
-		String equation = dialog.readLine("Z = ");
-		equation.replaceAll("\s+","");
+		String tempEquation = dialog.readLine("Z = ");
+		equation = tempEquation.replaceAll("\\s+","");
 		System.out.println(equation);
 		StringTokenizer st = new StringTokenizer(equation, DELIMITERS, true);
 		Stack<Operator> operators = new Stack<Operator>();
